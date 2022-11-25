373b37cb2f8960985211684a6ff6dc997a6c07b1
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-11-25 22:01:48 +0600

    user crud operation added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
 
-
+// payment for stripe
+const stripe = require('stripe')('sk_test_51Io76EFe9X02hyfxekpLXBLSGlNGu7IgPMBC0iI4akbawAtMr47vtwrmhi6hFgimTNDv0oT3gmUIduleJCYnNun700GqLD1XsA');
 
 //middleware
 const corsConfig = {
@@ -33,6 +34,8 @@ async function run() {
         const usersCollections = client.db('classified').collection('users');
         const categoryCollections = client.db('classified').collection('categories');
         const bookingsCollections = client.db('classified').collection('bookings');
+        const paymentsCollection = client.db('classified').collection('payments');
+        const wishlistCollection = client.db('classified').collection('wishlist');
         // console.log(productsCollections);
 
 
@@ -101,9 +104,39 @@ async function run() {
 
         });
 
+        // for sellers
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollections.findOne(query);
+            res.send({ isSeller: user?.role === 'Seller' });
+        });
+
+        // for admin
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollections.findOne(query);
+            res.send({ isAdmin: user?.role === 'Admin' });
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollections.insertOne(user);
+            res.send(result);
+        });
+
+        // for making admin 
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'Admin'
+                }
+            };
+            const result = await usersCollections.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
 
@@ -113,15 +146,76 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const booking = { _id: ObjectId(id) };
+            const result = await bookingsCollections.findOne(booking);
+            res.send(result);
+        });
+
         app.get('/bookings', async (req, res) => {
             const query = {};
             const result = await bookingsCollections.find(query).toArray();
             res.send(result);
         });
 
+
         app.post('/bookings', async (req, res) => {
             const product = req.body;
             const result = await bookingsCollections.insertOne(product);
+            res.send(result);
+        });
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const productprice = booking.productprice;
+            const amount = productprice * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                'payment_method_types': [
+                    'card'
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            });
+
+        });
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+
+                }
+            };
+            const updatedResult = await bookingsCollections.updateOne(filter, updatedDoc);
+            res.send({ result, updatedResult });
+        });
+
+        app.get('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const wishlist = { _id: ObjectId(id) };
+            const result = await wishlistCollection.findOne(wishlist);
+            res.send(result);
+        });
+
+        app.get('/wishlist', async (req, res) => {
+            const query = {};
+            const wishlist = await wishlistCollection.find(query).toArray();
+            res.send(wishlist);
+        });
+
+        app.post('/wishlist', async (req, res) => {
+            const wishlist = req.body;
+            const result = await wishlistCollection.insertOne(wishlist);
             res.send(result);
         });
 
