79a4f152a5b8a242acc6018ef64900755291c578
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-11-28 21:19:34 +0600

    final chnages updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const { query } = require('express');
 
 // payment for stripe
-const stripe = require('stripe')('sk_test_51Io76EFe9X02hyfxekpLXBLSGlNGu7IgPMBC0iI4akbawAtMr47vtwrmhi6hFgimTNDv0oT3gmUIduleJCYnNun700GqLD1XsA');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 //middleware
 const corsConfig = {
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xetzjun.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
@@ -35,7 +35,7 @@ function verifyJWT(req, res, next) {
         return res.status(401).send({ message: 'Unauthorized Access' });
     }
     const token = authHeader.split(' ')[1];
-    console.log(token);
+    // console.log(token);
     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
         if (err) {
             return res.status(403).send({ message: 'Access forbidden' });
@@ -57,6 +57,17 @@ async function run() {
         const wishlistCollection = client.db('classified').collection('wishlist');
         const adsCollection = client.db('classified').collection('ads');
         // console.log(productsCollections);
+
+        const verifyAdmin = async (req, res, next) => {
+            // console.log('inside of verifyAdmin', req.decoded.email);
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollections.findOne(query);
+            if (user?.role !== 'Admin') {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        };
 
 
         // jwt
@@ -139,7 +150,7 @@ async function run() {
 
         app.get('/products', verifyJWT, async (req, res) => {
             const decoded = req.decoded;
-            console.log(decoded);
+            // console.log(decoded);
             if (decoded.email !== req.query?.email) {
                 return res.status(403).send({ message: 'Unauthorized Access' });
             }
@@ -149,7 +160,7 @@ async function run() {
                     email: req.query?.email
                 };
             }
-            console.log(query);
+            // console.log(query);
             const cursor = productsCollections.find(query);
             const result = await cursor.toArray();
             res.send(result);
@@ -182,6 +193,13 @@ async function run() {
         app.post('/bookings', async (req, res) => {
             const product = req.body;
             const result = await bookingsCollections.insertOne(product);
+            res.send(result);
+        });
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await bookingsCollections.deleteOne(filter);
             res.send(result);
         });
 
@@ -221,7 +239,7 @@ async function run() {
 
         app.put('/users/seller/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
+            // console.log(id);
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
@@ -274,7 +292,7 @@ async function run() {
         });
 
         // for making admin 
-        app.put('/users/admin/:id', async (req, res) => {
+        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
@@ -301,9 +319,11 @@ async function run() {
         });
 
 
+
         app.post('/create-payment-intent', async (req, res) => {
             const booking = req.body;
             const productprice = booking.productprice;
+            // console.log(productprice);
             const amount = productprice * 100;
 
             const paymentIntent = await stripe.paymentIntents.create({
@@ -319,12 +339,50 @@ async function run() {
 
         });
 
+        app.post('/create-payment-intents', async (req, res) => {
+            const booking = req.body;
+            const productprice = booking.productprice;
+            // console.log(productprice);
+
+            const amount = 10 * 100;
+            // console.log(amount);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                'payment_method_types': [
+                    'card'
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            });
+
+        });
+
+        // app.put('/wishPayment', async (req, res) => {
+        //     const payment = req.body;
+        //     const result = await paymentsCollection.insertOne(payment);
+        //     const wishId = payment.bookingId;
+        //     const wishFilter = { bookingId: wishId };
+        //     console.log(wishFilter);
+        //     const wishUpdatedDoc = {
+        //         $set: {
+        //             paid: true,
+        //             availability: true,
+        //             transactionId: payment.transactionId,
+        //         }
+        //     };
+        //     const wishProducts = await wishlistCollection.updateOne(wishFilter, wishUpdatedDoc, options);
+        //     res.send({ result, wishProducts });
+        // });
+
         app.put('/payments', async (req, res) => {
             const payment = req.body;
             const result = await paymentsCollection.insertOne(payment);
             const id = payment.bookingsId;
             const filter = { _id: ObjectId(id) };
-            console.log('inside of payments', filter);
+            // console.log('inside of payments', filter);
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
@@ -346,7 +404,7 @@ async function run() {
             // for ads collection
             const ads = payment.bookingId;
             const adFilter = { bookingId: ads };
-            console.log(adFilter);
+            // console.log(adFilter);
             const adsUpdateDoc = {
                 $set: {
                     paid: true,
@@ -354,10 +412,25 @@ async function run() {
                     transactionId: payment.transactionId,
                 }
             };
+
+            // for wishlist
+
+            const wishId = payment.bookingId;
+            const wishFilter = { bookingId: wishId };
+            // console.log(wishFilter);
+            const wishUpdatedDoc = {
+                $set: {
+                    paid: true,
+                    availability: true,
+                    transactionId: payment.transactionId,
+                }
+            };
+
             const updatedResult = await bookingsCollections.updateOne(filter, updatedDoc, options);
             const myProducts = await productsCollections.updateOne(filters, updatedDocs, options);
             const adsProduct = await adsCollection.updateOne(adFilter, adsUpdateDoc, options);
-            res.send({ result, updatedResult, myProducts, adsProduct });
+            const wishProducts = await wishlistCollection.updateOne(wishFilter, wishUpdatedDoc, options);
+            res.send({ result, updatedResult, myProducts, adsProduct, wishProducts });
         });
         // app.put('/payment', async (req, res) => {
         //     const payment = req.body;
@@ -385,6 +458,7 @@ async function run() {
             res.send(result);
         });
 
+
         app.get('/wishlist', async (req, res) => {
             const query = {};
             const wishlist = await wishlistCollection.find(query).toArray();
@@ -394,6 +468,13 @@ async function run() {
         app.post('/wishlist', async (req, res) => {
             const wishlist = req.body;
             const result = await wishlistCollection.insertOne(wishlist);
+            res.send(result);
+        });
+
+        app.delete('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const wishlist = { _id: ObjectId(id) };
+            const result = await wishlistCollection.findOne(wishlist);
             res.send(result);
         });
 
